90fb5c58a0422f4b012717485558631ed35ae124
Maxime Danlee <maxime@Maximes-MacBook-Pro.local>
2023-12-26 18:10:54 +0100

    add adb in work directory

--- monitoring.py
+++ monitoring.py
@@ -3,7 +3,10 @@ import subprocess
 def run_command(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process.communicate()
-    return output.decode(), error.decode(), process.returncode
+    print(output.decode())
+    if error:
+        print(error.decode())
+    return process.returncode
 
 def get_temp_info():
     cpu_info_command = "cat sys/class/thermal/thermal_zone0/temp"
@@ -13,16 +16,26 @@ def get_temp_info():
 
 def run_stress_test_cpu():
     # push stress file to device
-    run_command("adb push cpu/stress-android/obj/local/armeabi-v7a/stress /data/local/tmp/stress")
+    code = run_command("platform-tools/adb push cpu/stress-android/obj/local/armeabi-v7a/stress /data/local/tmp/stress")
+    if code != 0:
+        print("Push stress file to device failed")
+        return
+    
     print("Push stress file to device successfully")
 
     # change permission
-    run_command("adb shell chmod 755 /data/local/tmp/stress")
+    code = run_command("platform-tools/adb shell chmod 755 /data/local/tmp/stress")
+    if code != 0:
+        print("Change permission failed")
+        return
 
     # run stress test
     print("Stress test is running...")
-    run_command("adb shell /data/local/tmp/stress -c 4 -t 10")
-    print("Run stress test successfully")
+    code = run_command("platform-tools/adb shell /data/local/tmp/stress -c 4 -t 10")
+    if code != 0:
+        print("Run stress test failed")
+        return
+    print("Run stress test successfully !!!")
 
 if __name__ == "__main__":
     run_stress_test_cpu()
