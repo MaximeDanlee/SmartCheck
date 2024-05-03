df41d380a5d7b4508aabb283fada07ccf1c9ba90
MaximeDanlee <max@interplanet.be>
2024-05-03 17:25:11 +0200

    Add port test

--- backend/app.py
+++ backend/app.py
@@ -48,7 +48,7 @@ if waiting is true, an exit_function must be defined
 """
 tests = {
     # "Cpu": {"function": test_cpu.main, "single": False, "waiting": False},
-    "Ports": {"function": test_port.main, "single": True, "waiting": False},
+    "USB Ports": {"function": test_port.main, "single": True, "waiting": False},
     # "4G": {"function": test_4g.main, "single": False, "waiting": False},
     # "Battery": {"function": test_battery.main, "single": False, "waiting": False},
     # "GPS": {"function": test_gps.main, "single": False, "waiting": False},
